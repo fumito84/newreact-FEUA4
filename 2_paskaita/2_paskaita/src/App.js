6514ab2214e2204bff0c525ff61974975f731be5
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-05-23 19:23:11 +0300

    2 paskaita baigta

--- 2_paskaita/2_paskaita/src/App.js
+++ 2_paskaita/2_paskaita/src/App.js
@@ -1,10 +1,16 @@
 import Car from "./components/Car";
 import FuncCar from "./components/FuncCar";
 import FuncBestCar from "./components/FuncBestCar";
+import Counter from "./components/Counter";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
+      <Counter />
+      <br />
+      <br />
+      <br />
       <h2>Class</h2>
       <Car />
       <br />
@@ -17,6 +23,7 @@ const App = () => {
       <br />
       <h2>Function with separate states</h2>
       <FuncBestCar />
+      <br />
     </div>
   );
 };
