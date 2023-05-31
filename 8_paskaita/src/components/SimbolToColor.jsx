94aaf96fcb185298f7d203c5215d8a068a2a3b32
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-05-31 21:39:46 +0300

    8 atnaujinta

--- 8_paskaita/src/components/SimbolToColor.jsx
+++ 8_paskaita/src/components/SimbolToColor.jsx
@@ -6,11 +6,6 @@ import "../components/SimbolToColor.css"
 function SimbolToColor() {
     const [text, setText] = useState('');
 
-    const handleInputChange = (e) => {
-        const inputValue = e.target.value;
-        setText(inputValue);
-    };
-
     const getBackgraundColor = (textLength) => {
         if (textLength < 3) {
             return 'white';
@@ -26,9 +21,9 @@ function SimbolToColor() {
     const backgraundColor = getBackgraundColor(text.length);
 
     return (
-        <div className='output' style={{backgraundColor}}>
+        <div className={backgraundColor}>
             <label>
-                <input type="text" value={text} onChange={handleInputChange} />
+                <input type="text" onChange={(e) => setText(e.target.value)} />
             </label>
         </div>
     );
