1afe1b1f932994ac48d24358b6e3f456be3ac223
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-06-06 20:19:24 +0300

    10 paskaita

--- 10_paskaita/src/App.js
+++ 10_paskaita/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Marketplace from "./pages/Marketplace";
-import NotFound from "../pages/NotFound";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
